1a876a674f24f6d419ab650bcbea06edd10ba057
soh335 <sugarbabe335@gmail.com>
2016-08-23 12:16:09 +0900

    use urfave/cli

--- main.go
+++ main.go
@@ -10,8 +10,7 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/codegangsta/cli"
-	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
 )
 
 func main() {
@@ -56,78 +55,68 @@ func main() {
 	app.Run(os.Args)
 }
 
-func CategoriesAction(c *cli.Context) {
-	f := func(c *cli.Context) error {
-		r, err := _fetch(c.GlobalString("url"))
-		if err != nil {
-			return err
-		}
-		defer r.Close()
-
-		doc, err := goquery.NewDocumentFromReader(r)
-		if err != nil {
-			return err
-		}
-
-		for _, category := range _parseCategories(doc) {
-			fmt.Println(category)
-		}
-		return nil
+func CategoriesAction(c *cli.Context) error {
+	r, err := _fetch(c.GlobalString("url"))
+	if err != nil {
+		return err
 	}
-	if err := f(c); err != nil {
-		logrus.Fatal(err)
+	defer r.Close()
+
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return err
 	}
+
+	for _, category := range _parseCategories(doc) {
+		fmt.Println(category)
+	}
+	return nil
 }
 
-func ICalAction(c *cli.Context) {
-	f := func(c *cli.Context) error {
-		categories := strings.Split(c.String("category"), ",")
-		if len(categories) < 1 {
-			return fmt.Errorf("required category")
-		}
-
-		whiteSpace := regexp.MustCompile(`\s`)
-		for _, category := range categories {
-			if whiteSpace.MatchString(category) {
-				return fmt.Errorf("should not contain white space in category")
-			}
-		}
-
-		loc, err := time.LoadLocation(c.String("tzid"))
-		if err != nil {
-			return err
-		}
-		now := time.Now().In(loc)
-
-		channels := strings.Split(c.String("channel"), ",")
-		if len(channels) < 1 {
-			return fmt.Errorf("required channel")
-		}
-
-		for i, channel := range channels {
-			channels[i] = strings.TrimSpace(channel)
-		}
-
-		r, err := _fetch(c.GlobalString("url"))
-		if err != nil {
-			return err
-		}
-		defer r.Close()
-
-		doc, err := goquery.NewDocumentFromReader(r)
-		if err != nil {
-			return err
-		}
-
-		cal, err := ICal(doc, categories, channels, now, c.String("calname"), c.Bool("liveonly"))
-		if err != nil {
-			return err
-		}
-		return cal.Encode(os.Stdout)
+func ICalAction(c *cli.Context) error {
+	categories := strings.Split(c.String("category"), ",")
+	if len(categories) < 1 {
+		return fmt.Errorf("required category")
 	}
-	if err := f(c); err != nil {
-		logrus.Fatal(err)
+
+	whiteSpace := regexp.MustCompile(`\s`)
+	for _, category := range categories {
+		if whiteSpace.MatchString(category) {
+			return fmt.Errorf("should not contain white space in category")
+		}
 	}
+
+	loc, err := time.LoadLocation(c.String("tzid"))
+	if err != nil {
+		return err
+	}
+	now := time.Now().In(loc)
+
+	channels := strings.Split(c.String("channel"), ",")
+	if len(channels) < 1 {
+		return fmt.Errorf("required channel")
+	}
+
+	for i, channel := range channels {
+		channels[i] = strings.TrimSpace(channel)
+	}
+
+	r, err := _fetch(c.GlobalString("url"))
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return err
+	}
+
+	cal, err := ICal(doc, categories, channels, now, c.String("calname"), c.Bool("liveonly"))
+	if err != nil {
+		return err
+	}
+	return cal.Encode(os.Stdout)
 }
 
 func _inslice(s string, strs []string) bool {
